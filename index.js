d91afec99a9095eb1967c6c7e59cef8e3d184fe7
Anand0047 <anandram221003@gmail.com>
2025-04-15 20:20:52 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Retrieve user data from local storage
+    const userDataString = localStorage.getItem('userData');
+    
+    if (userDataString) {
+        const userData = JSON.parse(userDataString);
+        
+        // Display user name in index.html
+        const userDataContainer = document.getElementById('userDataContainer');
+        userDataContainer.innerHTML = `
+            helloo <b>${userData.name}</b> 
+            and your email is <b> ${userData.email}</b></p>
+        `;
+    } else {
+        // If user data is not found, display a message
+        const userDataContainer = document.getElementById('userDataContainer');
+        userDataContainer.innerHTML = `<p style="text-decoration: none;" ><a href="./login/login.html">Please Sign-up !!!</a></p>`;
+    }
+    document.addEventListener('DOMContentLoaded', function() {
+        // Get the sign-in form
+        const signInForm = document.querySelector('.sign-in-container form');
+    
+        // Function to handle login
+        function handleSignIn(event) {
+            event.preventDefault(); // Prevent form submission
+    
+            // Get input values
+            const email = signInForm.querySelector('input[name="email"]').value;
+            const password = signInForm.querySelector('input[name="password"]').value;
+    
+            // Retrieve user data from local storage
+            const userDataString = localStorage.getItem('userData');
+    
+            if (userDataString) {
+                const userData = JSON.parse(userDataString);
+    
+                // Check if email and password match
+                if (email === userData.email && password === userData.password) {
+                    // Redirect to index.html
+                    window.location.href = '../project main/sampleindex.html';
+                } else {
+                    // Display error message if credentials are incorrect
+                    alert('Invalid email or password');
+                }
+            } else {
+                // Display error message if no user data found
+                alert('No user data found!');
+            }
+        }
+    
+        // Add event listener to the form for login submission
+        signInForm.addEventListener('submit', handleSignIn);
+    });
+    
+});
---
+++ product.js
@@ -0,0 +1,183 @@
+const wrapper = document.querySelector(".sliderWrapper");
+const menuItems = document.querySelectorAll(".menuItem");
+
+const products = [
+  {
+    id: 1,
+    title: "Air Force",
+    price: 119,
+    colors: [
+      {
+        code: "black",
+        img: "./img/air.png",
+      },
+      {
+        code: "darkblue",
+        img: "./img/air2.png",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Air Jordan",
+    price: 149,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/jordan.png",
+      },
+      {
+        code: "green",
+        img: "./img/jordan2.png",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Blazer",
+    price: 109,
+    colors: [
+      {
+        code: "lightgray",
+        img: "./img/blazer.png",
+      },
+      {
+        code: "green",
+        img: "./img/blazer2.png",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Crater",
+    price: 129,
+    colors: [
+      {
+        code: "black",
+        img: "./img/crater.png",
+      },
+      {
+        code: "lightgray",
+        img: "./img/crater2.png",
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Hippie",
+    price: 99,
+    colors: [
+      {
+        code: "gray",
+        img: "./img/hippie.png",
+      },
+      {
+        code: "black",
+        img: "./img/hippie2.png",
+      },
+    ],
+  },
+];
+
+let choosenProduct = products[0];
+
+const currentProductImg = document.querySelector(".productImg");
+const currentProductTitle = document.querySelector(".productTitle");
+const currentProductPrice = document.querySelector(".productPrice");
+const currentProductColors = document.querySelectorAll(".color");
+const currentProductSizes = document.querySelectorAll(".size");
+
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    //change the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //change the choosen product
+    choosenProduct = products[index];
+
+    //change texts of currentProduct
+    currentProductTitle.textContent = choosenProduct.title;
+    currentProductPrice.textContent = "$" + choosenProduct.price;
+    currentProductImg.src = choosenProduct.colors[0].img;
+
+    //assing new colors
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = choosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener("click", () => {
+    currentProductImg.src = choosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener("click", () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = "white";
+      size.style.color = "black";
+    });
+    size.style.backgroundColor = "black";
+    size.style.color = "white";
+  });
+});
+
+
+
+// const productButton = document.querySelector(".btn-buy");
+// const payment = document.querySelector(".payment");
+// const close = document.querySelector(".close");
+
+// productButton.addEventListener("click", () => {
+//   payment.style.display = "flex";
+// });
+
+// close.addEventListener("click", () => {
+//   payment.style.display = "none";
+// });
+
+
+// function addToCart(event) {
+//   const productInfo = {
+//       name: event.target.closest('.product').querySelector('.product-name').innerText.trim(),
+//       price: event.target.closest('.product').querySelector('.product-price:last-of-type').innerText.trim(),
+//   };
+
+//   let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+//   cart.push(productInfo);
+
+//   localStorage.setItem('cart', JSON.stringify(cart));
+
+//   alert('Product added to cart!');
+// }
+
+// const addToCartButtons = document.querySelectorAll('.btn-cart');
+// addToCartButtons.forEach(button => {
+//   button.addEventListener('click', addToCart);
+// });
+
+
+
+function addToCart(event) {
+  const productInfo = {
+      name: event.target.closest('.product').querySelector('.product-name').innerText.trim(),
+      price: event.target.closest('.product').querySelector('.product-price:last-of-type').innerText.trim(),
+
+  };
+
+  let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+
+  cart.push(productInfo);
+
+  sessionStorage.setItem('cart', JSON.stringify(cart));
+
+  swal("Good job!", "Product added to cart 😍", "success");
+}
+
+const addToCartButtons = document.querySelectorAll('.btn-cart');
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', addToCart);
+});
